b38e07626cd94490f8158e79b7d92ba5a96aa48a
Jeffrey Myers <JeffM2501@gmail.com>
2018-07-30 21:23:40 -0700

    use OpenTK vectors start port of AL to use OpenTKAL.

---
+++ Engine/Math/MathHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LudicrousElectron.Engine.Math
+{
+    public static class MathHelper
+    {
+        internal static readonly float MachineEpsilonFloat = GetMachineEpsilonFloat();
+
+        public static float ToDegrees (float rad)
+        {
+            return rad * (float)(180 / System.Math.PI);
+        }
+
+        internal static bool WithinEpsilon(float floatA, float floatB)
+        {
+            return System.Math.Abs(floatA - floatB) < MachineEpsilonFloat;
+        }
+
+        private static float GetMachineEpsilonFloat()
+        {
+            float machineEpsilon = 1.0f;
+            float comparison;
+
+            /* Keep halving the working value of machineEpsilon until we get a number that
+			 * when added to 1.0f will still evaluate as equal to 1.0f.
+			 */
+            do
+            {
+                machineEpsilon *= 0.5f;
+                comparison = 1.0f + machineEpsilon;
+            }
+            while (comparison > 1.0f);
+
+            return machineEpsilon;
+        }
+    }
+}
--- LudicrousElectron.Types/Rect2Df.cs
+++ LudicrousElectron.Types/Rect2Df.cs
@@ -4,25 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OpenTK;
+
 namespace LudicrousElectron.Types
 {
 	public class Rect2Df
 	{
-		public Vector2f LowerBound = new Vector2f();
-		public Vector2f UpperBound = new Vector2f();
+		public Vector2 LowerBound = new Vector2();
+		public Vector2 UpperBound = new Vector2();
 
 		public Rect2Df() { }
 
 		public Rect2Df(float x, float y, float width, float height)
 		{
-			LowerBound = new Vector2f(x, y);
-			UpperBound = new Vector2f(x + width, y + height);
+			LowerBound = new Vector2(x, y);
+			UpperBound = new Vector2(x + width, y + height);
 		}
 
-		public Rect2Df(Vector2f origin, Vector2f size)
+		public Rect2Df(Vector2 origin, Vector2 size)
 		{
-			LowerBound = new Vector2f(origin.x, origin.x);
-			UpperBound = new Vector2f(origin.x + size.x, origin.x + size.y);
+			LowerBound = new Vector2(origin.X, origin.X);
+			UpperBound = new Vector2(origin.X + size.X, origin.Y + size.Y);
 		}
 	}
 }
